cab2c1fd56961185ed9cb60a6d3e6ca57cf57dbd
DanielDT <daniel@fintual.com>
2021-05-25 16:08:07 -0400

    feat() Add base endpoints for interview interaction

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
+app.get('/', function (req, res) {
+  const count = 0;
+
+  res.send(`Current count is ${count}.`)
+})
+
+app.get('/increment/:count', function (req, res) {
+  const count = req.params.count;
+
+  res.send(`Count incremeted by ${count}`)
+})
+
 app.get('/ping', function (req, res) {
   res.send('pong')
 })
